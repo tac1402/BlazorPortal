2e668441c02f24e1126d44b96dc50ad99a82907a
Sergey <tac1402@gmail.com>
2026-04-04 02:13:30 +0300

    -empty modules

--- src/BlazorWorld.Web.Client.Modules/BlazorWorld.Web.Client.Modules.Admin/Route.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorWorld.Web.Client.Modules.Admin
-{
-    // empty class for identifying assembly in BlazorWorld.Web.Client's
-    // App.razor, Router component, AdditionalAssemblies attribute.
-    public class Route
-    {
-    }
-}
